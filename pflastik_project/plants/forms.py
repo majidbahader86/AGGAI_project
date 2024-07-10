cc4281fcbd047de22acb1d03d682e1b3d98665dd
Majid Bahader <majid.bahader@dci-student.org>
2024-07-11 01:17:09 +0200

    views, forms, urls, templates

---
+++ pflastik_project/plants/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Plant, PlantImage
+
+class PlantForm(forms.ModelForm):
+    class Meta:
+        model = Plant
+        fields = ['name', 'scientific_name', 'description', 'habitat']
+
+class PlantImageForm(forms.ModelForm):
+    class Meta:
+        model = PlantImage
+        fields = ['image', 'description', 'is_healthy']
