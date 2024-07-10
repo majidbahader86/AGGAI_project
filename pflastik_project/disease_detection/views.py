0cde8cdd00adbf708e0be0d970d4854133becd94
Majid Bahader <majid.bahader@dci-student.org>
2024-07-10 11:23:25 +0200

    added views

--- pflastik_project/disease_detection/views.py
+++ pflastik_project/disease_detection/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+# views.py
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import DiseaseCategory, PlantPart, DiagnosticSession
+
+# DiseaseCategory views
+def disease_category_list(request):
+    disease_categories = DiseaseCategory.objects.all()
+    return render(request, 'disease_category_list.html', {'disease_categories': disease_categories})
+
+def disease_category_detail(request, pk):
+    disease_category = get_object_or_404(DiseaseCategory, pk=pk)
+    return render(request, 'disease_category_detail.html', {'disease_category': disease_category})
+
+# PlantPart views
+def plant_part_list(request):
+    plant_parts = PlantPart.objects.all()
+    return render(request, 'plant_part_list.html', {'plant_parts': plant_parts})
+
+def plant_part_detail(request, pk):
+    plant_part = get_object_or_404(PlantPart, pk=pk)
+    return render(request, 'plant_part_detail.html', {'plant_part': plant_part})
+
+# DiagnosticSession views
+def diagnostic_session_list(request):
+    diagnostic_sessions = DiagnosticSession.objects.all()
+    return render(request, 'diagnostic_session_list.html', {'diagnostic_sessions': diagnostic_sessions})
+
+def diagnostic_session_detail(request, pk):
+    diagnostic_session = get_object_or_404(DiagnosticSession, pk=pk)
+    return render(request, 'diagnostic_session_detail.html', {'diagnostic_session': diagnostic_session})
