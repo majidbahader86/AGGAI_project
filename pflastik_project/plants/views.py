2015fa4c451d4e0b3bb03cac82f027effbf4671c
Majid Bahader <majid.bahader@dci-student.org>
2024-07-10 11:30:33 +0200

    views added

--- pflastik_project/plants/views.py
+++ pflastik_project/plants/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# views.py
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Plant, PlantImage
+
+# Plant views
+def plant_list(request):
+    plants = Plant.objects.all()
+    return render(request, 'plant_list.html', {'plants': plants})
+
+def plant_detail(request, pk):
+    plant = get_object_or_404(Plant, pk=pk)
+    return render(request, 'plant_detail.html', {'plant': plant})
+
+# PlantImage views
+def plant_image_list(request, plant_id):
+    plant = get_object_or_404(Plant, pk=plant_id)
+    images = plant.images.all()
+    return render(request, 'plant_image_list.html', {'plant': plant, 'images': images})
+
+def plant_image_detail(request, plant_id, image_id):
+    image = get_object_or_404(PlantImage, pk=image_id)
+    return render(request, 'plant_image_detail.html', {'image': image})
