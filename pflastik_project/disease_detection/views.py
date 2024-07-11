11c021905d3845373b92b58bceb77b34b5bb0d96
Majid Bahader <majid.bahader@dci-student.org>
2024-07-11 22:51:43 +0200

    views

--- pflastik_project/disease_detection/views.py
+++ pflastik_project/disease_detection/views.py
@@ -1,14 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Create Disease Identification Request</title>
-</head>
-<body>
-    <h2>Create Disease Identification Request</h2>
-    <form method="POST" enctype="multipart/form-data">
-        {% csrf_token %}
-        {{ form.as_p }}
-        <button type="submit">Submit</button>
-    </form>
-</body>
-</html>
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import DiseaseIdentificationRequest
+from .forms import DiseaseIdentificationRequestForm
+
+# View to create a new disease identification request
+@login_required
+def create_disease_identification_request(request):
+    if request.method == 'POST':
+        form = DiseaseIdentificationRequestForm(request.POST, request.FILES)
+        if form.is_valid():
+            disease_request = form.save(commit=False)
+            disease_request.user = request.user
+            disease_request.save()
+            return redirect('list_disease_identification_requests')
+    else:
+        form = DiseaseIdentificationRequestForm()
+    return render(request, 'create_disease_identification_request.html', {'form': form})
+
+# View to list all disease identification requests for the logged-in user
+@login_required
+def list_disease_identification_requests(request):
+    disease_requests = DiseaseIdentificationRequest.objects.filter(user=request.user)
+    return render(request, 'list_disease_identification_requests.html', {'disease_requests': disease_requests})
