9cf0b8ffb58678b3da352f625914cbe27ddac491
Nina <catriona.hynes@dci-student.org>
2024-07-11 09:42:38 +0200

    forms_ext

--- pflastik_project/farmers/forms.py
+++ pflastik_project/farmers/forms.py
@@ -1,17 +1,26 @@
-# forms.py
-
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from .models import DiseaseIdentificationRequest, ForumPost, ForumComment, SeasonAlert
 
 class FarmerSignUpForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-class FarmerLoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.is_farmer = True  # Assuming you have a boolean field is_farmer in your User model
+        if commit:
+            user.save()
+        return user
+
+class FarmerLoginForm(AuthenticationForm):
+    def confirm_login_allowed(self, user):
+        if not user.is_active or not user.is_farmer:
+            raise forms.ValidationError(
+                "There was an error with your login.",
+                code='invalid_login',
+            )
