fb27dc82c18474286a487b214fb6dce0b71a5cd0
Majid Bahader <majid.bahader@dci-student.org>
2024-07-15 15:16:36 +0200

    updated

--- pflastik_project/accounts/models.py
+++ pflastik_project/accounts/models.py
@@ -1,3 +1,4 @@
+# accounts/models.py
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -12,7 +13,7 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=User, weak=False)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
