bdba31d6fd4e16da7d8637c1fb96a7f39ed2c5b6
Majid Bahader <majid.bahader@dci-student.org>
2024-07-15 15:26:19 +0200

    updated views

--- pflastik_project/AI_tools/admin.py
+++ pflastik_project/AI_tools/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import AITool, AIQuestion, AIAnswer, AIToolUsage, Conversation
+
+# Register your models here
+@admin.register(AITool)
+class AIToolAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+@admin.register(AIQuestion)
+class AIQuestionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'question', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('user__username', 'question')
+
+@admin.register(AIAnswer)
+class AIAnswerAdmin(admin.ModelAdmin):
+    list_display = ('question', 'answer')
+    search_fields = ('question__question',)
+
+@admin.register(AIToolUsage)
+class AIToolUsageAdmin(admin.ModelAdmin):
+    list_display = ('tool', 'user', 'usage_date')
+    list_filter = ('usage_date',)
+    search_fields = ('tool__name', 'user__username')
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'llm_name', 'timestamp')
+    list_filter = ('timestamp',)
+    search_fields = ('user__username', 'llm_name')
--- pflastik_project/AI_tools/views.py
+++ pflastik_project/AI_tools/views.py
@@ -1,89 +1,106 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
-from .forms import AIToolForm, AIQuestionForm, AIAnswerForm, AIModelForm, AIResultForm
-from .models import AITool, AIQuestion, AIAnswer, AIModel, AIResult
+# views.py
 
-# AI Tool Views
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import AITool, AIQuestion, AIAnswer, AIToolUsage
+from .forms import AIToolForm, AIQuestionForm, AIAnswerForm, AIToolUsageForm
+
+# AITool views
+def ai_tool_list(request):
+    ai_tools = AITool.objects.all()
+    return render(request, 'ai_tool_list.html', {'ai_tools': ai_tools})
+
+def ai_tool_detail(request, pk):
+    ai_tool = get_object_or_404(AITool, pk=pk)
+    return render(request, 'ai_tool_detail.html', {'ai_tool': ai_tool})
+
 @login_required
-def create_ai_tool(request):
+def ai_tool_create(request):
     if request.method == 'POST':
         form = AIToolForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('ai_tool_list')
+            ai_tool = form.save()
+            return redirect('ai_tool_detail', pk=ai_tool.pk)
     else:
         form = AIToolForm()
-    return render(request, 'create_ai_tool.html', {'form': form})
+    return render(request, 'ai_tool_form.html', {'form': form})
 
 @login_required
-def ai_tool_list(request):
-    tools = AITool.objects.all()
-    return render(request, 'ai_tool_list.html', {'tools': tools})
+def ai_tool_update(request, pk):
+    ai_tool = get_object_or_404(AITool, pk=pk)
+    if request.method == 'POST':
+        form = AIToolForm(request.POST, request.FILES, instance=ai_tool)
+        if form.is_valid():
+            ai_tool = form.save()
+            return redirect('ai_tool_detail', pk=ai_tool.pk)
+    else:
+        form = AIToolForm(instance=ai_tool)
+    return render(request, 'ai_tool_form.html', {'form': form, 'ai_tool': ai_tool})
 
-# AI Question Views
+# AIQuestion views
 @login_required
-def create_ai_question(request):
+def ai_question_list(request):
+    questions = AIQuestion.objects.filter(user=request.user)
+    return render(request, 'ai_question_list.html', {'questions': questions})
+
+@login_required
+def ai_question_detail(request, pk):
+    question = get_object_or_404(AIQuestion, pk=pk)
+    return render(request, 'ai_question_detail.html', {'question': question})
+
+@login_required
+def ai_question_create(request):
     if request.method == 'POST':
         form = AIQuestionForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('ai_question_list')
+            question = form.save(commit=False)
+            question.user = request.user
+            question.save()
+            return redirect('ai_question_detail', pk=question.pk)
     else:
         form = AIQuestionForm()
-    return render(request, 'create_ai_question.html', {'form': form})
+    return render(request, 'ai_question_form.html', {'form': form})
+
+# AIAnswer views
+@login_required
+def ai_answer_detail(request, question_id):
+    answer = get_object_or_404(AIAnswer, question_id=question_id)
+    return render(request, 'ai_answer_detail.html', {'answer': answer})
 
 @login_required
-def ai_question_list(request):
-    questions = AIQuestion.objects.all()
-    return render(request, 'ai_question_list.html', {'questions': questions})
-
-# AI Answer Views
-@login_required
-def create_ai_answer(request):
+def ai_answer_create(request, question_id):
+    question = get_object_or_404(AIQuestion, pk=question_id)
     if request.method == 'POST':
         form = AIAnswerForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('ai_answer_list')
+            answer = form.save(commit=False)
+            answer.question = question
+            answer.save()
+            return redirect('ai_question_detail', pk=question.pk)
     else:
         form = AIAnswerForm()
-    return render(request, 'create_ai_answer.html', {'form': form})
+    return render(request, 'ai_answer_form.html', {'form': form, 'question': question})
+
+# AIToolUsage views
+@login_required
+def ai_tool_usage_list(request):
+    tool_usages = AIToolUsage.objects.filter(user=request.user)
+    return render(request, 'ai_tool_usage_list.html', {'tool_usages': tool_usages})
 
 @login_required
-def ai_answer_list(request):
-    answers = AIAnswer.objects.all()
-    return render(request, 'ai_answer_list.html', {'answers': answers})
+def ai_tool_usage_detail(request, pk):
+    tool_usage = get_object_or_404(AIToolUsage, pk=pk)
+    return render(request, 'ai_tool_usage_detail.html', {'tool_usage': tool_usage})
 
-# AI Model Views
 @login_required
-def create_ai_model(request):
+def ai_tool_usage_create(request):
     if request.method == 'POST':
-        form = AIModelForm(request.POST, request.FILES)
+        form = AIToolUsageForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('ai_model_list')
+            tool_usage = form.save(commit=False)
+            tool_usage.user = request.user
+            tool_usage.save()
+            return redirect('ai_tool_usage_detail', pk=tool_usage.pk)
     else:
-        form = AIModelForm()
-    return render(request, 'create_ai_model.html', {'form': form})
-
-@login_required
-def ai_model_list(request):
-    models = AIModel.objects.all()
-    return render(request, 'ai_model_list.html', {'models': models})
-
-# AI Result Views
-@login_required
-def create_ai_result(request):
-    if request.method == 'POST':
-        form = AIResultForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('ai_result_list')
-    else:
-        form = AIResultForm()
-    return render(request, 'create_ai_result.html', {'form': form})
-
-@login_required
-def ai_result_list(request):
-    results = AIResult.objects.all()
-    return render(request, 'ai_result_list.html', {'results': results})
+        form = AIToolUsageForm()
+    return render(request, 'ai_tool_usage_form.html', {'form': form})
