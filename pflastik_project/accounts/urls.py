f193fd533c49e1ad8901ec3d0d2d12c0cfe1e871
Majid Bahader <majid.bahader@dci-student.org>
2024-07-16 14:45:14 +0200

    urls accounts

--- pflastik_project/accounts/urls.py
+++ pflastik_project/accounts/urls.py
@@ -3,10 +3,8 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('profile/', views.update_user_profile, name='profile'),
-    path('profile/update_profile/', views.update_profile, name='update_profile'),
-    path('profile/update_user/', views.update_user, name='update_user'),
+    path('update_user/<int:user_id>/', views.update_user, name='update_user'),
+    path('update_profile/<int:profile_id>/', views.update_profile, name='update_profile'),
+    path('update_foo/<int:foo_id>/', views.update_foo, name='update_foo'),
 ]
-
 
