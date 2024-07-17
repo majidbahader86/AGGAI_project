5c197d9c4fc5a5391255fb4f4025a1eb27bbaa25
Nina <catriona.hynes@dci-student.org>
2024-07-17 13:49:31 +0200

    accounts_updated

--- pflastik_project/accounts/urls.py
+++ pflastik_project/accounts/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),  # Example traditional view
     path('profile/', views.profile_view, name='profile'),
-    path('api/', include('account.api.urls')),  # Include API URLs
+    path('update/user/<int:user_id>/', views.update_user, name='update_user'),
+    path('update/profile/<int:profile_id>/', views.update_profile, name='update_profile'),
+    path('update/foo/<int:foo_id>/', views.update_foo, name='update_foo'),
+    path('foo/', include('foo.urls')),  
 ]
