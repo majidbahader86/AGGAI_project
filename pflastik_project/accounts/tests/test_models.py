7fdcc3d1fae2bc0b3531e819e2b99735798a0a1e
Nina <catriona.hynes@dci-student.org>
2024-07-17 14:42:43 +0200

    tests

--- pflastik_project/accounts/tests/test_models.py
+++ pflastik_project/accounts/tests/test_models.py
@@ -1,59 +1,22 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from ..models import Profile, Foo
+from accounts.models import Profile, Foo
 
 class ProfileModelTest(TestCase):
-
-    def setUp(self):
-        self.user = User.objects.create(username='testuser')
-        self.profile = Profile.objects.create(user=self.user, is_farmer=True)
-
     def test_profile_creation(self):
-        """Test profile is created correctly"""
-        self.assertEqual(self.profile.user.username, 'testuser')
-        self.assertTrue(self.profile.is_farmer)
-        self.assertFalse(self.profile.is_scientist)
-
-    def test_profile_update(self):
-        """Test updating profile fields"""
-        self.profile.is_scientist = True
-        self.profile.save()
-        updated_profile = Profile.objects.get(id=self.profile.id)
-        self.assertTrue(updated_profile.is_scientist)
+        user = User.objects.create(username='testuser')
+        profile = Profile.objects.get(user=user)
+        self.assertEqual(profile.user.username, 'testuser')
+        self.assertFalse(profile.is_farmer)
+        self.assertFalse(profile.is_scientist)
 
 class FooModelTest(TestCase):
-
-    def setUp(self):
-        self.foo = Foo.objects.create(user='F')
-
     def test_foo_creation(self):
-        """Test foo is created correctly"""
-        self.assertEqual(str(self.foo), 'Farmer')
+        foo = Foo.objects.create(user='S')
+        self.assertEqual(foo.user, 'S')
+        self.assertEqual(foo.__str__(), 'Scientist')
 
-    def test_foo_update(self):
-        """Test updating foo instance"""
-        self.foo.user = 'S'
-        self.foo.save()
-        updated_foo = Foo.objects.get(id=self.foo.id)
-        self.assertEqual(str(updated_foo), 'Scientist')
-
-    def test_foo_choices(self):
-        """Test user choices in Foo model"""
-        choices = dict(Foo.USER_CHOICES)
-        self.assertEqual(choices['S'], 'Scientist')
-        self.assertEqual(choices['F'], 'Farmer')
-
-    def test_signal_create_profile(self):
-        """Test signal creates profile for new user"""
-        new_user = User.objects.create(username='newuser')
-        self.assertTrue(Profile.objects.filter(user=new_user).exists())
-
-    def test_signal_update_profile(self):
-        """Test signal updates existing profile for user"""
-        self.user = User.objects.create(username='existinguser')
-        self.user.profile.is_farmer = True
-        self.user.save()
-        updated_profile = Profile.objects.get(user=self.user)
-        self.assertTrue(updated_profile.is_farmer)
-
+        foo = Foo.objects.create(user='F')
+        self.assertEqual(foo.user, 'F')
+        self.assertEqual(foo.__str__(), 'Farmer')
 
--- pflastik_project/accounts/urls.py
+++ pflastik_project/accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('update/user/<int:user_id>/', views.update_user, name='update_user'),
     path('update/profile/<int:profile_id>/', views.update_profile, name='update_profile'),
     path('update/foo/<int:foo_id>/', views.update_foo, name='update_foo'),
-    path('foo/', include('foo.urls')),  
+   
 ]
