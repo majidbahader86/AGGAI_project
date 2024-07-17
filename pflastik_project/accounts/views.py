5d4bba1f3f4b8582b1d8d9f5326eb676d922df4f
Majid Bahader <majid.bahader@dci-student.org>
2024-07-17 13:10:00 +0200

    updated views for index

--- pflastik_project/accounts/views.py
+++ pflastik_project/accounts/views.py
@@ -1,32 +1,44 @@
-from django.shortcuts import render, redirect
+# account/views.py
+
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import UserForm, ProfileForm, FooForm
 from .models import Profile, Foo
+from django.contrib.auth.models import User
+
+def index(request):
+    # Example index view
+    return render(request, 'index.html', {})
+
+def profile_view(request):
+    # Example profile view
+    # Assuming you have logic to retrieve and display user profile
+    return render(request, 'profile.html', {})
 
 def update_user(request, user_id):
-    user = user.objects.get(pk=user_id)
+    user = get_object_or_404(User, pk=user_id)
     if request.method == 'POST':
         user_form = UserForm(request.POST, instance=user)
         if user_form.is_valid():
             user_form.save()
-            return redirect('profile_detail', user_id=user.id)  # Replace with the appropriate URL name for profile detail view
+            return redirect('profile_detail', user_id=user.id)
     else:
         user_form = UserForm(instance=user)
     return render(request, 'update_user.html', {'user_form': user_form})
 
 def update_profile(request, profile_id):
-    profile = Profile.objects.get(pk=profile_id)
+    profile = get_object_or_404(Profile, pk=profile_id)
     if request.method == 'POST':
         profile_form = ProfileForm(request.POST, instance=profile)
         if profile_form.is_valid():
             profile_form.save()
-            return redirect('profile_detail', user_id=profile.user.id)  # Replace with the appropriate URL name for profile detail view
+            return redirect('profile_detail', user_id=profile.user.id)
     else:
         profile_form = ProfileForm(instance=profile)
     return render(request, 'update_profile.html', {'profile_form': profile_form})
 
 def update_foo(request, foo_id=None):
     if foo_id:
-        foo = Foo.objects.get(pk=foo_id)
+        foo = get_object_or_404(Foo, pk=foo_id)
     else:
         foo = None
 
@@ -34,7 +46,7 @@ def update_foo(request, foo_id=None):
         foo_form = FooForm(request.POST, instance=foo)
         if foo_form.is_valid():
             foo_form.save()
-            return redirect('foo_list')  # Replace with the appropriate URL name for foo list view
+            return redirect('foo_list')
     else:
         foo_form = FooForm(instance=foo)
     return render(request, 'update_foo.html', {'foo_form': foo_form})
